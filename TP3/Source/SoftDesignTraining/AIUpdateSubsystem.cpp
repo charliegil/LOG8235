1668611942f49b82e748c07246ba154d28a90841
Your Name <maxence.lhuisset@polymtl.ca>
2025-11-20 14:07:49 -0500

    finished and test P4

---
+++ TP3/Source/SoftDesignTraining/AIUpdateSubsystem.cpp
@@ -0,0 +1,52 @@
+#include "C:\Users\maxen\Documents\LOG8235\LOG8235\TP3\Intermediate\Build\Win64\x64\SoftDesignTrainingEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "AIUpdateSubsystem.h"
+#include "SDTAIController.h"
+#include "HAL/PlatformTime.h"
+
+void UAIUpdateSubsystem::RegisterAgent(ASDTAIController* AI)
+{
+	Agents.Add(AI);
+}
+
+void UAIUpdateSubsystem::UnregisterAgent(ASDTAIController* AI)
+{
+	Agents.Remove(AI);
+}
+
+void UAIUpdateSubsystem::Tick(float DeltaTime)
+{
+	TimeUsedThisFrame = 0.0;
+
+	if (Agents.Num() == 0)
+		return;
+
+	// Récupère l'index du premier agent pas updated lors du dernier tick pour commencer par celui ci dans la liste
+	int32 Count = Agents.Num();
+	int32 StartIndex = NextAgentIndex;
+
+	for (int i = 0; i < Count; ++i)
+	{
+		int32 Index = (StartIndex + i) % Count;
+
+		ASDTAIController* AI = Agents[Index];
+		if (!AI)
+			continue;
+
+		bool bInsideBudgetNow = (TimeUsedThisFrame < FrameBudgetMS);
+
+		if (!bInsideBudgetNow)
+		{
+			NextAgentIndex = Index;
+			return;
+		}
+
+		double Start = FPlatformTime::Seconds();
+		AI->UpdateAIWithBudget(DeltaTime, true);
+		double End = FPlatformTime::Seconds();
+
+		TimeUsedThisFrame += (End - Start) * 1000.0;
+	}
+	NextAgentIndex = 0;
+}
+
+
